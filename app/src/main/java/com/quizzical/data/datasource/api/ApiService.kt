9c4eedbe9ad401cd3b08b84335e14ef6bd2bcd0b
Debora Deotti <debora.deotti@ifood.com.br>
2025-05-28 21:17:42 -0300

    Update to 5 questions

--- app/src/main/java/com/quizzical/data/datasource/api/ApiService.kt
+++ app/src/main/java/com/quizzical/data/datasource/api/ApiService.kt
@@ -1,10 +1,9 @@
 package com.quizzical.data.datasource.api
 
 import com.quizzical.data.datasource.dto.QuizResponseDTO
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("api.php?amount=10&type=multiple")
+    @GET("api.php?amount=5&type=multiple")
     suspend fun getQuestions(): QuizResponseDTO
 }
