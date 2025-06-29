5394e3c368b856a54210f9b7c277f97b1c505a1f
Debora Deotti <debora.deotti@ifood.com.br>
2025-06-29 19:07:47 -0300

    fix start screen layout

--- app/src/main/java/com/quriozzity/presentation/start/view/StartScreen.kt
+++ app/src/main/java/com/quriozzity/presentation/start/view/StartScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -76,8 +77,9 @@ fun StartContent(
             painter = painterResource(R.drawable.green_blob),
             contentDescription = null,
             modifier = Modifier
+                .fillMaxWidth()
                 .align(Alignment.TopStart)
-                .size(600.dp)
+                .height(600.dp)
                 .offset(y = (-200).dp)
                 .zIndex(0f)
         )
